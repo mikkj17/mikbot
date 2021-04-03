b12d99802f13a8b932a6ab6ef84f94b5391d1dc0
Mikkel <mikkj17@student.sdu.dk>
2021-04-03 20:22:24 +0200

    Add motivation. Need to fix possible ffmpeg error

--- mikbot.py
+++ mikbot.py
@@ -24,26 +24,34 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
+
 @bot.command()
 async def troels(ctx):
     quote, context = random.choice(utils.parse_troels())
     await ctx.send(f'{quote}\n*{context}*')
 
+
 @bot.command()
-async def silke(ctx, name: str=None):
+async def silke(ctx, name: str = None):
     if name is not None:
         await ctx.send(f'This one is for you, {name.title()}', tts=True)
         time.sleep(3)
     await utils.play_mp3(ctx, 'silke')
 
+
 @bot.command()
 async def hestene(ctx):
     await utils.play_mp3(ctx, 'hestene')
+
 
 @bot.command()
 async def ja(ctx):
     await utils.play_mp3(ctx, 'ja')
 
+
+@bot.command()
+async def motivation(ctx):
+    await utils.play_mp3(ctx, 'motivation')
 
 
 bot.run(TOKEN)
--- utils.py
+++ utils.py
@@ -1,19 +1,28 @@
 import discord
 import time
 
+FFMPEG_OPTIONS = {
+    'options': '-vn'
+}
+
 def parse_troels():
     with open('resources/citater.txt', encoding='utf-8') as f:
         quotes = f.read().split('\n\n\n')
     return [quote.split('\n\n') for quote in quotes]
 
+async def join(ctx, channel):
+    if ctx.voice_client is not None:
+        return await ctx.voice_client.move_to(channel)
+    
+    await channel.connect()
+
 async def play_mp3(ctx, filename):
-    voice = ctx.author.voice
-    if voice is not None:
-        vc = await voice.channel.connect()
-        vc.play(discord.FFmpegPCMAudio(source=f'resources/{filename}.mp3'))
-        while vc.is_playing():
-            time.sleep(.1)
-        await vc.disconnect()
+    voice_channel = ctx.author.voice.channel
+    await join(ctx, voice_channel)
+    ctx.voice_client.play(discord.FFmpegPCMAudio(source=f'resources/{filename}.mp3', **FFMPEG_OPTIONS))
+    while ctx.voice_client.is_playing():
+        time.sleep(0.1)
+    await ctx.voice_client.disconnect()
 
 if __name__ == '__main__':
     quotes = parse_troels()
