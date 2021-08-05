8f33fe6b6ffcd20c93bc20a8335e31edc6b11ba7
Mikkel <mikkj17@student.sdu.dk>
2021-08-06 00:04:26 +0200

    add initial functionality for livescore

--- mikbot.py
+++ mikbot.py
@@ -3,11 +3,13 @@ import os
 import random
 import time
 import utils
-
-from discord.ext import commands
 from discord import Intents
+from discord.ext import commands
+from discord.ext.commands.context import Context
 from dotenv import load_dotenv
 from typing import Optional
+
+import livescore
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -28,13 +30,13 @@ async def on_ready():
 
 
 @bot.command()
-async def troels(ctx):
+async def troels(ctx: Context):
     chosen = random.choice(quotes)
     await ctx.send(f'{chosen["quote"]}\n*{chosen["context"]}*')
 
 
 @bot.command()
-async def silke(ctx, channel: Optional[discord.VoiceChannel], member: str = None):
+async def silke(ctx: Context, channel: Optional[discord.channel.VoiceChannel], member: str = None):
     if member:
         await ctx.send(f'Den her går ud til dig, {member.title()}', tts=True)
         time.sleep(3)
@@ -42,18 +44,41 @@ async def silke(ctx, channel: Optional[discord.VoiceChannel], member: str = None
 
 
 @bot.command()
-async def hestene(ctx, channel: Optional[discord.VoiceChannel]):
+async def hestene(ctx: Context, channel: Optional[discord.channel.VoiceChannel]):
     await utils.play_mp3(ctx, 'hestene', channel)
 
 
 @bot.command()
-async def ja(ctx, channel: Optional[discord.VoiceChannel]):
+async def ja(ctx: Context, channel: Optional[discord.channel.VoiceChannel]):
     await utils.play_mp3(ctx, 'ja', channel)
 
 
 @bot.command()
-async def motivation(ctx, channel: Optional[discord.VoiceChannel]):
+async def motivation(ctx: Context, channel: Optional[discord.channel.VoiceChannel]):
     await utils.play_mp3(ctx, 'motivation', channel)
 
-bot.run(TOKEN)
 
+#TODO: make equivalent unsubscribe function, or better, merge both into one function calling right functions with getattr etc.
+@bot.command()
+async def subscribe(ctx: Context, team_name: str):
+    possible_teams = livescore.search_for_team(team_name)
+
+    if possible_teams.count() == 0:
+        message = 'Team not found!'
+
+    elif possible_teams.count() > 1:
+        formatted_teams = '\n'.join(team['name'] for team in possible_teams)
+        message = f'Multiple teams found. Please specify one of the following:\n{formatted_teams}'
+
+    else:
+        team = possible_teams[0]
+        update = livescore.subscribe(ctx.author.id, team)
+        if update.upserted_id is None and update.modified_count == 0:
+            message = f"You're already subscribed to {team['name']} :soccer:"
+        else:
+            message = f"You're now subscribed to {team['name']} :soccer:"
+
+    await ctx.send(message)
+
+
+bot.run(TOKEN)
--- utils.py
+++ utils.py
@@ -1,16 +1,19 @@
 import discord
 import json
 import time
-
 from string import Template
 from typing import Dict
 from typing import List
+
+from discord.ext.commands.context import Context
+
 
 MP3_ERROR_MSG = Template(
 """Hey $name. Du er noob!
 Gå ind i en voice channel eller skriv \
 navnet på den kanal hvor jeg skal \
 afspille **$filename** :wink:.""")
+
 
 def log(func):
     def inner(*args, **kwargs):
@@ -19,19 +22,22 @@ def log(func):
         return func(*args, **kwargs)
     return inner
 
+
 def load_troels() -> List[Dict[str, str]]:
     with open('resources/citater.json', encoding='utf-8') as f:
         return json.load(f)
 
-async def connect_and_play(stream, channel):
+
+async def connect_and_play(stream: discord.player.FFmpegPCMAudio, channel: discord.channel.VoiceChannel) -> None:
     vc = await channel.connect()
     vc.play(stream)
     while vc.is_playing():
         time.sleep(.1)
     await vc.disconnect()
 
+
 @log
-async def play_mp3(ctx, filename, channel):
+async def play_mp3(ctx: Context, filename: str, channel: discord.channel.VoiceChannel) -> None:
     stream = discord.FFmpegPCMAudio(source=f'resources/{filename}.mp3')
     voice = ctx.author.voice
     if channel:
@@ -43,6 +49,7 @@ async def play_mp3(ctx, filename, channel):
             MP3_ERROR_MSG.substitute(name=ctx.author.name, filename=filename),
             mention_author=False
         )
+
 
 if __name__ == '__main__':
     quotes = load_troels()
